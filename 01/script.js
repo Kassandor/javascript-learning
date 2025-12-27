109f710d20b37b685e230edc354f26eb92c06c00
Kassandor <webmasterdracarys@gmail.com>
2025-12-27 17:57:40 +0300

    Поигрались с логами

---
+++ 01/script.js
@@ -0,0 +1,4 @@
+console.log('Внешний файл подключен');
+console.table({'name': 'alex', 'age': 10});
+const styles = 'background-color: lightseagreen; color: black; padding: 10px';
+console.log('%cСтилизованный лог', styles);
