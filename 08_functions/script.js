d2f9d52bcafb17f8227fccf2006c0eeeee904ef4
Kassandor <webmasterdracarys@gmail.com>
2026-05-13 12:36:22 +0300

    8 урок: функции

---
+++ 08_functions/script.js
@@ -0,0 +1,17 @@
+const log = console.log
+
+function sayHello(){
+    log('Hello!');
+}
+
+sayHello();
+
+
+// параметры и аргументы
+// параметры: num1, num2
+function sum(num1, num2){
+    log(num1 + num2);
+}
+// аргументы: 10, 20
+sum(10, 20)
+
